50d05bc1274e2580e44cbd6837e8f16c96c14659
Brian <brianjordan283@gmail.com>
2026-03-27 16:26:08 +0100

    write function for obtaining computer's choice

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const getComputerChoice = () => {
+    let compChoice = ""
+    let randNum = Math.floor(Math.random() * 3)
+    if (randNum == 0) {
+        compChoice = "rock"
+    } else if (randNum == 1) {
+        compChoice = "paper"
+    } else if (randNum == 2) {
+        compChoice = "scissors"
+    }
+    return compChoice
+}
+
+const userChoice = window.prompt("Please select your weapon")
