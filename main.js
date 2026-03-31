3bc15818c70a8d436c79d081d35ca9b48b3dad91
Brian <brianjordan283@gmail.com>
2026-03-31 15:04:55 +0200

    add buttons and functions for toggling visibility

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const banner = document.querySelector(".banner");
-
+const options = document.querySelectorAll('.option');
+const answers = document.querySelectorAll('.answer');
+const rock = document.querySelector(".rock")
+const yesButton = document.querySelector(".yes")
+console.log(yesButton)
+let showingOptions = true;
 const getComputerChoice = () => {
     let computerChoice = "";
     let randNum = Math.floor(Math.random() * 3);
@@ -16,55 +21,15 @@ const getComputerChoice = () => {
     return computerChoice;
 };
 
-const getUserChoice = () => {
-    const userChoice = window.prompt("rock, paper, scissors?");
-    return userChoice;
-}
+const toggleOptions = () => {
+    showingOptions = !showingOptions;
 
-const gamePrompt = () => {
-    const wantToPlay = window.prompt("Would you like to play rock, paper, scissors? Type yes or no")
-    if (wantToPlay == "yes") {
-        playGame();
-    } else {
-        console.log("ok bye")
-    }
-}
+    options.forEach(option => {
+        option.style.display = showingOptions ? 'inline' : 'none';
+    });
 
-const playGame = () => {
-    let result = "";
-    const compChoice = getComputerChoice();
-    const userChoice = getUserChoice();
-
-    //check if tie
-    if (userChoice == compChoice) {
-        result += "It's a tie."
-    }
-
-    //check if computer chose rock
-    if (compChoice == "rock") {
-      if (userChoice == "scissors") {
-        result += "You lose."
-      } else if (userChoice == "paper") {
-        result += "You win."
-      }
-    }
-    //check if computer chose scissors
-    if (compChoice == "scissors") {
-      if (userChoice == "paper") {
-        result += "You lose."
-      } else if (userChoice == "rock") {
-        result += "You win."
-      }
-    }
-    //check if computer chose paper
-    if (compChoice == "paper") {
-        if (userChoice == "rock") {
-            result += "You lose."
-        } else if (userChoice == "scissors") {
-            result += "You win."
-        }
-    }
-    console.log(result)
-
-}
-
+    answers.forEach(answer => {
+        answer.style.display = showingOptions ? 'none' : 'inline';
+    });
+};
+yesButton.addEventListener('click', toggleOptions);
